66e689f82f2b55c18a68b39b54c524dd531c45a0
Shubham Goel <shubham145@hotmail.com>
2016-10-22 15:35:49 +0100

    Time and Time/plus pages

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,8 +3,12 @@ from django.conf.urls import url
 from django.contrib import admin
 from mysite.views import hello
 from mysite.views import home
+from mysite.views import current_datetime, hours_ahead
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^hello/$', hello),
-    url(r'^$', home)
+    url(r'^$', home),
+    url(r'^time/$', current_datetime),
+    url(r'^time/plus/(\d{1,2})/$', hours_ahead),
+
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,7 +1,22 @@
 from django.http import HttpResponse
+import datetime
 
 def home(request):
     return HttpResponse("Shubham's Django Journey")
 
 def hello(request):
     return HttpResponse("Hello World")
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "The current date-time is %s" % now
+    return HttpResponse(html)
+
+def hours_ahead(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    html = "The Time in %s hour(s), it will be %s" % (offset, dt)
+    return HttpResponse(html)
